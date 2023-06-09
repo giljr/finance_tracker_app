c08767ff6c617630d183e3a192dfa15cf97fbba7
j3 <giljr.2009@gmail.com>
2023-06-09 19:09:50 -0400

    clean stock.rb

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,12 +2,6 @@ class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
             publishable_token: Rails.application.credentials.iex_client[:access_api_key],
-            # publishable_token: 'pk_b***12860e3f42b38e971943***',
-
-            # Instructions: video 257 - Secure Credential - Mashrur Hossain 
-            # run: EDITOR="code --wait" rails credentials:edit 
-            # save this key inside the https://www.vaultproject.io/
-            # https://stackoverflow.com/questions/59993437/can-i-generate-a-new-config-master-key-file
             endpoint: 'https://cloud.iexapis.com/v1'
           )
           begin
@@ -15,11 +9,5 @@ class Stock < ApplicationRecord
           rescue => exception
             return nil            
           end
-        # client.price(ticker_symbol)
-        #   new(ticker:ticker_symbol, name:client.company(ticker_symbol).company_name, last_price:client.price(ticker_symbol))
-
-
-        
-
     end
 end
