3fa2a2b772fee22d30aced8724eb4d0ea399a9fb
j3 <giljr.2009@gmail.com>
2023-06-16 23:05:46 -0400

    :recycle: refactor: Devise Sign-up & Registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def list_stocks
 
         @tracked_stocks = current_user.stocks
@@ -11,6 +13,14 @@ class ApplicationController < ActionController::Base
         # p "_______________________"
 
     end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    end
+    
 
 end
 
